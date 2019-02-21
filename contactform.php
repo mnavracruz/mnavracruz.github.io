b1dc4970e88d6333b1d0407e92885680a8fded42
Magdalena Navracruz <navracruz.m@husky.neu.edu>
2019-02-21 09:45:52 -0500

    Create contactform.php

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
+  $mailFrom = $_POST['email'];
+  $message = $_POST['message'];
+
+  $mailTo = "mnavracruz@jhancock.com";
+  $headers = "From: ".$mailFrom;
+  $txt = "You have received a message from ".$name.".\n\n".$message;
+
+  mail($mailTo, $subject, $txt, $headers);
+  header("Location: contact.html?mailsend");
+}
